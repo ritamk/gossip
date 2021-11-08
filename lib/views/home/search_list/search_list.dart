42d36738252049508ff8f0f77e54540f69029656
ritamk <ritamkonar7@gmail.com>
2021-11-08 19:46:31 +0530

    removing unused import

--- lib/views/home/search_list/search_list.dart
+++ lib/views/home/search_list/search_list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gossip/models/food.dart';
 import 'package:gossip/services/database.dart';
 import 'package:gossip/shared/buttons.dart';
